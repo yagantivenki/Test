724ee19aaf1d671efe4266c50be233af954d60bb
sathya <sathya@DESKTOP-FD90NSO>
2020-10-30 11:51:19 +0530

    iam venkates create statemt project complete

---
+++ CreateStatement/src/com/app/createstatement/StateMentDemo.java
@@ -0,0 +1,19 @@
+package com.app.createstatement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StateMentDemo {
+
+	public static void main(String[] args)throws SQLException
+	{
+		DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","admin");
+		Statement st=con.createStatement();
+		System.out.println("statement is created");
+
+	}
+
+}
